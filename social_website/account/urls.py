c8d07cbf29393763db3e659ae65356c192fad8f0
shahid <shahid.ice.nstu@gmail.com>
2021-12-02 10:50:11 +0600

    add password change feature

--- social_website/account/urls.py
+++ social_website/account/urls.py
@@ -9,5 +9,16 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(),
+        name='password_change',
+    ),
+    # password change urls
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done',
+    ),
     path('', views.dashboard, name='dashboard'),
 ]
