19c7d0b5a53e6ca5949d7c20114dc6ed6b8218f8
shahid <shahid.ice.nstu@gmail.com>
2021-12-05 10:51:07 +0600

    download image with given title and url'

---
+++ social_website/images/urls.py
@@ -0,0 +1,9 @@
+# images/urls.py
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('create/', views.image_create, name='create'),
+]
--- social_website/images/views.py
+++ social_website/images/views.py
@@ -1,3 +1,26 @@
+# images/views.py
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(data=request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            new_item = form.save(commit=False)
+
+            # assign current user to the item
+            new_item.user = request.user
+            new_item.save()
+            return HttpResponse("image saved successfully")
+    else:
+        form = ImageCreateForm(data=request.GET)
+
+    return render(
+        request, 'images/image/create.html', {'section': 'images', 'form': form}
+    )
