c8576ac6f6e0127218d115fae0de10ae10dac06a
shahid <shahid.ice.nstu@gmail.com>
2022-05-08 11:07:33 +0600

    add Profile Model

--- social_website/account/admin.py
+++ social_website/account/admin.py
@@ -1,3 +1,9 @@
+# social_website/account/admin.py
+
+from account.models import Profile
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
