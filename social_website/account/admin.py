4e47e172cec1320f365a9e7a76800133d7cc4b16
shahid <shahid.ice.nstu@gmail.com>
2022-05-16 15:09:58 +0600

    Add Contact model

--- social_website/account/admin.py
+++ social_website/account/admin.py
@@ -1,9 +1,12 @@
 # social_website/account/admin.py
 
-from account.models import Profile
+from account.models import Contact, Profile
 from django.contrib import admin
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
+
+
+admin.site.register(Contact)
